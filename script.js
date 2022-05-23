c53312bbbf344f5480e8dcfed14a7ecaeb4ac175
ABDULALI3468 <mrabdulali2003@gmail.com>
2022-05-23 06:59:32 -0700

    Solved the del button problem!

--- script.js
+++ script.js
@@ -2,101 +2,50 @@ let c = console.log;
 const title = document.querySelector(".title_input");
 const author = document.querySelector(".author_input");
 const registeredBooks = document.querySelector(".registered_books");
-const button = document.querySelector(".add-book");
+const addButton = document.querySelector(".add-book");
 
-const books = [];
+
+let books = [];
 let localstorage = JSON.parse(localStorage.getItem("books"));
-c(localstorage);
 
-// window.onload=load()
-
-// function load() {
-//   let localstorage = JSON.parse(localStorage.getItem("books"));
-//   for (let i = 0; localstorage.length; i += 1) {
-//     registeredBooks.innerHTML += `
-//   <div>
-//     <p class="title">${localstorage[i].title}</p>
-//     <p class="author">${localstorage[i].author}</p>
-//     <button del-id=${
-//       localstorage[i].id
-//     } class="button" onclick="del(event)">remove</button>
-//     <hr/>
-//   </div>
-//   `;
-//   }
-// }
 
 window.onload = () => {
-  let localstorage = JSON.parse(localStorage.getItem("books"));
-  if (localStorage.getItem("books")){
-    for (let i = 0; i<localstorage.length; i += 1) {
-      registeredBooks.innerHTML += `
-    <div>
-      <p class="title">${localstorage[i].title}</p>
-      <p class="author">${localstorage[i].author}</p>
-      <button del-id=${localstorage[i].id} class="button" onclick="del(event)">remove</button>
-      <hr/>
-    </div>
-    `;
-    }
+  if (localStorage.getItem("books")) {
+    books = JSON.parse(localStorage.getItem("books"));
   }
+  displayBook();
 };
 
-button.addEventListener("click", () => {
+
+addButton.addEventListener("click", () => {
   let book = {
     title: title.value,
     author: author.value,
-    id: Math.floor(Math.random() * 9999999999),
   };
   books.push(book);
-
-  registeredBooks.innerHTML += `
-  <div>
-    <p class="title">${book.title}</p>
-    <p class="author">${book.author}</p>
-    <button del-id=${book.id} class="button" onclick="del(event)">remove</button>
-    <hr/>
-  </div>
-  `;
-
-  title.value = "";
-  author.value = "";
+  displayBook();
   localStorage.setItem("books", JSON.stringify(books));
 });
 
-function del(e) {
-  c("bindas")
-  
-  let id = e.target.getAttribute("del-id");
-  c(id)
-  c(books.length)
-  c(books)
-  let local = JSON.parse(localStorage.getItem("books"));
-  for (let i = 0; i < local.length; i += 1) {
-    c(i)
-    if (local[i].id === Number(id)) {
-      c("yes")
-      books.splice(i, 1);
-      localStorage.setItem("books", JSON.stringify(books));
-    }
-    else{
-      c("no")
-    }
-  }
 
-  e.target.parentNode.remove();
+function displayBook() {
+  registeredBooks.innerHTML = '';
+  for (let i = 0; i < books.length; i += 1) {
+  registeredBooks.innerHTML += `
+  <div>
+    <p class="title">${books[i].title}</p>
+    <p class="author">${books[i].author}</p>
+    <button class="button" onclick="remove(${i})">remove</button>
+    <hr/>
+  </div>
+  `;
+    title.value = "";
+    author.value = "";
+  }
 }
 
-// function load() {
-//   let localstorage = JSON.parse(localStorage.getItem("books"));
-//   for (let i = 0; localstorage.length; i += 1) {
-//     registeredBooks.innerHTML += `
-//   <div>
-//     <p class="title">${localstorage[i].title}</p>
-//     <p class="author">${localstorage[i].author}</p>
-//     <button del-id=${localstorage[i].id} class="button" onclick="del(event)">remove</button>
-//     <hr/>
-//   </div>
-//   `;
-//   }
-// }
+function remove(index) {
+  books.splice(index, 1);
+  displayBook();
+  localStorage.setItem("books", JSON.stringify(books));
+}
