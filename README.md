c910ec46066ba9a20e4b1bd0f97e39542889d4cd
Jahongir Yusupov <91022355+jahongiry@users.noreply.github.com>
2022-05-23 19:29:09 +0500

    Add linters files

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
+![](https://img.shields.io/badge/Microverse-blueviolet)
+
 # AWESOME-BOOKS-PROJECT
+
+In this project, We set up a "AWESOME-BOOKS" repository.
+
+## Built With
+
+- HTML
+- Javascript
+
+## Getting Started
+
+### Download it as a ZIP file
+
+or
+
+### Clone it in your local machine using GIT
+
+To get a local copy up and running follow these simple example steps.
+Clone the repo
+   git clone https://github.com/ABDULALI3468/AWESOME-BOOKS-PROJECT.git
+
+## Author
+
+👤 Jahongir Yusupov
+
+- GitHub: [https://github.com/jahongiry](https://github.com/jahongiry)
+- LinkedIn: [linkedin.com/in/jahongir-yusupov](linkedin.com/in/jahongir-yusupov)
+
+👤 ABDULALI3468
+
+- GitHub: [https://github.com/jahongiry](https://github.com/ABDULALI3468)
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+## Show your support
+
+Give a ⭐️ if you like this project!
--- script.js
+++ script.js
@@ -1,51 +1,45 @@
-let c = console.log;
-const title = document.querySelector(".title_input");
-const author = document.querySelector(".author_input");
-const registeredBooks = document.querySelector(".registered_books");
-const addButton = document.querySelector(".add-book");
-
+const title = document.querySelector('.title_input');
+const author = document.querySelector('.author_input');
+const registeredBooks = document.querySelector('.registered_books');
+const addButton = document.querySelector('.add-book');
 
 let books = [];
-let localstorage = JSON.parse(localStorage.getItem("books"));
-
-
-window.onload = () => {
-  if (localStorage.getItem("books")) {
-    books = JSON.parse(localStorage.getItem("books"));
-  }
-  displayBook();
-};
-
-
-addButton.addEventListener("click", () => {
-  let book = {
-    title: title.value,
-    author: author.value,
-  };
-  books.push(book);
-  displayBook();
-  localStorage.setItem("books", JSON.stringify(books));
-});
-
 
 function displayBook() {
   registeredBooks.innerHTML = '';
   for (let i = 0; i < books.length; i += 1) {
-  registeredBooks.innerHTML += `
-  <div>
-    <p class="title">${books[i].title}</p>
-    <p class="author">${books[i].author}</p>
-    <button class="button" onclick="remove(${i})">remove</button>
-    <hr/>
-  </div>
-  `;
-    title.value = "";
-    author.value = "";
+    registeredBooks.innerHTML += `
+    <div>
+      <p class="title">${books[i].title}</p>
+      <p class="author">${books[i].author}</p>
+      <button class="button" onclick="remove(${i})">remove</button>
+      <hr/>
+    </div>
+   `;
+    title.value = '';
+    author.value = '';
   }
 }
 
 function remove(index) {
   books.splice(index, 1);
   displayBook();
-  localStorage.setItem("books", JSON.stringify(books));
+  localStorage.setItem('books', JSON.stringify(books));
 }
+
+window.onload = () => {
+  if (localStorage.getItem('books')) {
+    books = JSON.parse(localStorage.getItem('books'));
+  }
+  displayBook();
+};
+
+addButton.addEventListener('click', () => {
+  const book = {
+    title: title.value,
+    author: author.value,
+  };
+  books.push(book);
+  displayBook();
+  localStorage.setItem('books', JSON.stringify(books));
+});
