f442d73fcc4d9427ba8ccd0ce78051aa32984e9d
Jahongir Yusupov <91022355+jahongiry@users.noreply.github.com>
2022-05-25 14:34:00 +0500

    Add styles and time

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ const title = document.querySelector('.title_input');
 const author = document.querySelector('.author_input');
 const registeredBooks = document.querySelector('.registered_books');
 const addButton = document.querySelector('.add-book');
+const addNew = document.querySelector('.add-new');
+const contact = document.querySelector('.contact');
+const contactInfo = document.querySelector('.contact-info');
+const list = document.querySelector('.list');
+const addForm = document.querySelector('.add-form ');
+const date = document.querySelector('.date');
+const updateHeading = document.getElementById('heading');
+const dataTime = new Date();
 
 if (localStorage.getItem('books')) {
   if (localStorage.getItem('books').length > 2) {
@@ -17,12 +25,12 @@ class BookClass {
     this.books = [];
   }
 
-  addbook = (book) => {
+  addbook = book => {
     books.push(book);
     localStorage.setItem('books', JSON.stringify(books));
   };
 
-  removeBook = (index) => {
+  removeBook = index => {
     books.splice(index, 1);
     localStorage.setItem('books', JSON.stringify(books));
   };
@@ -85,4 +93,27 @@ addButton.addEventListener('click', () => {
       registeredBooks.classList.add('registered_books2');
     }
   }
+});
+
+date.innerHTML = dataTime;
+
+addNew.addEventListener('click', () => {
+  addForm.style.display = 'block';
+  registeredBooks.style.display = 'none';
+  contactInfo.style.display = 'none';
+  updateHeading.innerHTML = 'Add a New Book';
+});
+
+contact.addEventListener('click', () => {
+  contactInfo.style.display = 'block';
+  registeredBooks.style.display = 'none';
+  addForm.style.display = 'none';
+  updateHeading.innerHTML = 'Contact Information';
+});
+
+list.addEventListener('click', () => {
+  registeredBooks.style.display = 'block';
+  contactInfo.style.display = 'none';
+  addForm.style.display = 'none';
+  updateHeading.innerHTML = 'All awesome books';
 });
